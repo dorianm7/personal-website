b38d7707f60ef8cd046b8bbcfa80e3bea9bd8e86
Dorian Maldonado <d1maldon@ucsd.edu>
2020-07-25 12:09:56 -0700

    Connect addButton to open dialog

--- public/scripts/crud.js
+++ public/scripts/crud.js
@@ -1,3 +1,18 @@
 import {createBlogDialog} from './blog.js';
 
 //will add listeners from crud.html in here
+var dialog;
+
+const setUpAddButton = () => {
+    let addButton = document.getElementById('add-button');
+    addButton.addEventListener('click', () => {
+        dialog = createBlogDialog();
+        document.body.appendChild(dialog);
+        dialog.showModal();
+    });
+};
+
+//Wire up elements here
+document.addEventListener('DOMContentLoaded', () => {
+    setUpAddButton();
+});
