1955c27f52688acdcf2f6ca17c039eb9e773f474
Dorian Maldonado <d1maldon@ucsd.edu>
2020-07-25 15:26:12 -0700

    Create blog post from dialog and add to table and array

--- public/scripts/crud.js
+++ public/scripts/crud.js
@@ -1,7 +1,6 @@
-import {createBlogDialog} from './blog.js';
+import {createBlogDialog, createBlogPost} from './blog.js';
 
 //will add listeners from crud.html in here
-var dialog;
 var blogs = [{title: 'Title1', date: '10/11/12', summary: 'Summary1'},
             {title: 'Title2', date: '10/11/12', summary: 'Summary2'}];
 
@@ -14,34 +13,66 @@ const createTableRow = () => {
                             .cloneNode(true);
 };
 
+//calls setUpAddButton
 const setUpAddButton = () => {
     let addButton = document.getElementById('add-button');
     addButton.addEventListener('click', () => {
+        let dialog = createBlogDialog();
+        document.body.appendChild(dialog);
+        setUpDialog(dialog);
         dialog.showModal();
     });
 };
 
-const setUpDialog = () => {
-    dialog = createBlogDialog();
-    document.body.appendChild(dialog);
-
+//called inside of setUpAddButton
+const setUpDialog = (dialog) => {
     //set up listeners
     let cancelButton = dialog.querySelector('#cancel');
     cancelButton.addEventListener('click', () => {
         dialog.close(false);
+        //remove dialog from document
+        dialog.parentNode.removeChild(dialog);
     });
     
     let okButton = dialog.querySelector('#save');
     okButton.addEventListener('click', () => {
-        dialog.close(false);
         //get title, date, summary 
-        //blogs.push({title, date, summary})
-        //place {title, date, summary} to table
+        let titleEl = dialog.querySelector('#title'); 
+        let timeEl = dialog.querySelector('#date');
+        let summaryEl = dialog.querySelector('#summary');
+        let titleString = titleEl.value;
+        let dateString = timeEl.value;
+        let summaryString = summaryEl.value;
+
+        //create blog object
+        let blog = {title: titleString, date: dateString, summary: summaryString};
+
+        //add blog object to array
+        blogs.push(blog);
+
+        //place blog post to table
+        blogToTable(blog);
+
+        //remove dialog from document
+        dialog.close(false);
+        dialog.parentNode.removeChild(dialog);
     });
+};
+
+const blogToTable = (blog) => {
+    let blogPost = createBlogPost(blog.title, blog.date, blog.summary);
+    let blogTableRow = createTableRow();
+    let blogTableRowTD = blogTableRow.querySelector('td');
+
+    blogTableRowTD.appendChild(blogPost);
+    
+    let table = document.getElementById('blogs');
+    table.appendChild(blogTableRow);
 };
 
 //Wire up elements here
 document.addEventListener('DOMContentLoaded', () => {
-    setUpDialog();
     setUpAddButton();
+    //read array
+    //place array elements into table
 });
