92e4e40bb6bab1ecff94c6381998480e7afae9e5
Dorian Maldonado <d1maldon@ucsd.edu>
2020-07-29 17:54:13 -0700

    Implement download resume button

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,6 @@
-// Placeholder script for now
+window.addEventListener('DOMContentLoaded', () => {
+    let resumeBtn = document.getElementById('download-resume-button');
+    resumeBtn.addEventListener('click', () => {
+        window.open('../Dorian_Alexis_Maldonado_resume.pdf', '_blank');
+    });
+});
