51dc6faea0e63795eed619e14903899bc0ef92b4
Dorian Alexis Maldonado <dorianmaldonado7@gmail.com>
2022-08-09 13:24:39 -0700

    Fix imports

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
     let resumeBtn = document.getElementById('download-resume-button');
     resumeBtn.addEventListener('click', () => {
-        window.open('/public/Dorian_Alexis_Maldonado_resume.pdf', '_blank');
+        window.open('./Dorian_Alexis_Maldonado_resume.pdf', '_blank');
     });
 });
